6b7fbe1f661f9b4eacc606735a592dd65ff0a30f
Sarah Jane Jones <sarahjanejones@manjaro.localdomain>
2021-02-03 12:40:17 +0200

    Neatened up code and changed comments based on improved understanding. Fixed an error in slb_edit_post_change_title function where it was possible to attempt to access ->post_type where post_type does not exist

--- my-udemy-snappy-list-builder.php
+++ my-udemy-snappy-list-builder.php
@@ -11,21 +11,37 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-// FILTERS 
+// Hooks 
 // Filter hooks allow us to get data from WordPress, modify it, and return it back customized
 // Whereas action hooks let us run our own code when a certain event takes place in the WordPress lifecycle
 
+// The filter manage_{}_columns is one that is made available by WordPress for  custom post types
+// This filter has a dynamic portion in the hook name (edit-slb_subscriber) which is the $screen->id (can see this using inspect eleent on the subscriber page)
+add_filter('manage_edit-slb_subscriber_columns','slb_subscriber_column_headers');
+add_filter('manage_edit-slb_list_columns','slb_list_column_headers');
+// do_action( "add_meta_boxes_{$post_type}", WP_Post $post )
+// Fires after all built-in meta boxes have been added, contextually for the given post type.
+// Once built in metaboxes have been added the code here adds the custom slb-subscriber-details metabox
+add_action('add_meta_boxes_slb_subscriber','slb_add_subscriber_metaboxes');
+add_action('save_post', 'slb_save_slb_subscriber_meta', 10, 3);
+// admin_head - Fires in head section for all admin pages.
+add_action('admin_head','slb_edit_post_change_title');
+
+
+
+//Functions
+
+// Registers custom admin column headers
 // $columns is an associative array that contains information about the column headers
-// I presume it must be passed in automatically?? 
- // Is called using a hook (below)
+// It is accessible becasue it is an argument of the filter.
  function slb_subscriber_column_headers( $columns ) {
 	
 	// creating custom column header data - i.e. override data that is coming in through the argument
 	$columns = array(
-		// cb - check box - empty here - keeps them there but empty (unchecked)
+		// cb - check box - keeps them there but empty (unchecked)
 		'cb'=>'<input type="checkbox" />',
 		// __() translates - 
-		// Changes title from Title to Subscriber name
+		// Changes title from Title to Subscriber Name
 		'title'=>__('Subscriber Name')
 	);
 	
@@ -34,39 +50,27 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 	
 }
 
-// Registers custom admin column headers
-// The filter manage_edit-slb_subscriber_columns is one that WordPress already generated for our custom post type
-// Still don't understand how these instance specific action and filter hooks work or when they created ??
-// This filter has a dynamic portion in the hook name (edit-slb_subscriber) which is the $screen->id (can see this using inspect eleent on the subscriber page)
-add_filter('manage_edit-slb_subscriber_columns','slb_subscriber_column_headers');
 
-
-//nSam as above but for the custom Lists post type
+//Same as above but for the custom Lists post type
  function slb_list_column_headers( $columns ) {
 	
 	// creating custom column header data - i.e. override data that is coming in through the argument
 	$columns = array(
-		// cb - check box - empty here - keeps them there but empty (unchecked)
+		// cb - check box - keeps them there but empty (unchecked)
 		'cb'=>'<input type="checkbox" />',
 		// __() translates - 
-		// Changes title from Title to Subscriber name
+		// Changes title from Title to Subscriber Lists
 		'title'=>__('Subscriber Lists')
 	);
 	
 	// returning new columns
 	return $columns;
-	
+
 }
-
-add_filter('manage_edit-slb_list_columns','slb_list_column_headers');
-
-// MISC
 
 // A post meta box is a draggable box shown on the post editing screen. 
 // Its purpose is to allow the user to select or enter information in addition to the main post content.
-
 // These are the custom fields which appear on the custom post types, in this case the subscriber post type
-
 function slb_add_subscriber_metaboxes(){
 
 	// Built in WordPress function
@@ -81,27 +85,22 @@ function slb_add_subscriber_metaboxes(){
 
 }
 
-// ?? Not really sure what this action hook is or where it was set
-// Maybe add_meta_box function creates such an event based on its own name plus the slug??
-add_action('add_meta_boxes_slb_subscriber','slb_add_subscriber_metaboxes');
-
 // Creates the html for the custom fields in the metabox
-// Called inside the add_meta_box function
+// Specified inside the add_meta_box function inside the slb_add_subscriber_metaboxes function 
 function slb_subscriber_metabox(){
-	/* wp_nounce is a security measure for database to protect from attacks 
-	The nonce field is used to validate that the contents of the form came from the location on the current site 
-	and not somewhere else. 
-	?? nounce doesn't seem to change for me
+	/* wp_nounce is a security measure for a database to protect from attacks 
+	The nonce field is used to validate that the contents of the form came from the location on the current site and not somewhere else. 
+	?? The course explains the nounce value changes as you refresh your page and add new subscribers but when using inspect element it doesn't seem to change for me - this might indicate a problem, I am not sure.
 	*/
 	// First argument specifies an action - __FILE__ applies to the file you are currently in -somehow using the page we are on as an action ??
 	// The basename() function in PHP is an inbuilt function which is used to return the base name of a file 
 	// if the path of the file is provided as a parameter to the function. 
 	// E.g. basename("/etc/sudoers.d"); returns sudoers.d 
-	// It specifies the path of the file. $suffix: It is an optional parameter which hides the extension of a file if it ends with a suffix.
-	// Second argument specifies a name for it - used to refer back to later.
-	//
+	// Second argument specifies the name for the field - used to refer back to it later.
+	
 	wp_nonce_field( basename(__FILE__), 'slb_subscriber_nonce' );
-	//
+	
+	// I still don't understand pulling the global variable into the function scope??
 	global $post;
 	$post_id = $post->ID;
 
@@ -113,31 +112,12 @@ function slb_subscriber_metabox(){
 	?>
 
 	<style>
-		.slb-field-row {
-			display: flex;
-			flex-flow: row nowrap;
-		}
-		.slb-field-container {
-			position: relative;
-			flex: 1 1;
-			margin-right: 1em;
-		}
-		.slb-field-container label {
-			font-weight: bold;
-		}
 		.slb-field-container label span {
 			color: red;
 		}
-		.slb-field-container label ul {
-			list-style: none;
-			margin-top:0;
-		}
-		.slb-field-container label ul label {
-			font-weight: normal;
-		}
 	</style>
 
-	<div class="slb-field-row">
+	<div>
 		<div class="slb-field-container">
 			<p>
 			<label>First Name <span>*</span></label>
@@ -167,28 +147,27 @@ function slb_subscriber_metabox(){
 			?>" />
 			</p>
 		</div>
-		<div class="slb-field-container">
+		<div>
 			<p>
 			<label>Lists</label>
 			<ul>
 				<?php
 				
-				// ?? Still don't fully understand this, why are we declaring it, does it not exist already??
+				// ?? Still don't understand pulling global variables into the function's scope
 				// $wpdb is the WordPress database access abstraction class.
 				global $wpdb;
 
-				//So $list_query must be a table with column names, rows and values
-				// slb_list is the custom defined post type
-				// The query will return the ID's (system generated) and titles (specified by admin) 
-				// of all custom list posts (like list 1 - list4)
+				//$list_query is an array. Each index key has a value which is an object which contains a list ID and a list name (post_title). must be a table with column names, rows and values
+				// The IDs are system generated and titles are specified by admin
+				// Note $wpdb->posts not wp_posts as seen in the databse??
 				$list_query = $wpdb->get_results("SELECT ID, post_title FROM {$wpdb->posts} WHERE post_type = 'slb_list' AND post_status IN ('publish','draft')");
-
+				
 				if( !is_null($list_query) ) {
 					
 					// Each $list is a row from the table and has an ID and a post_title as per the query
 					foreach($list_query as $list ){
 						$checked = (in_array($list->ID, $lists)) ? 'checked="checked" ' : '';
-						// slb_list is the name of the custom post type which is why slb_list[] is needed to define the input of the lists which will be an array
+						// slb_list is the name of the custom post type, slb_list[] is the name of the array that holds the lists the user checked.
 						echo '<li><label><input type="checkbox" name="slb_list[]" value="'. $list->ID .'" '.$checked.' /> '. $list->post_title .'</label></li>';
 
 					}
@@ -203,19 +182,20 @@ function slb_subscriber_metabox(){
 	<?php
 }
 
-//adding data to databse - capture fields from our form and save them after they have been submitted
-// What are these $post and $post_type variables, when do they get passed in and how do they work?
+// Adding data to databse - capture fields from our form and save them after they have been submitted
+// $post_id, $post, $update are passed as parameters through the save_post hook.
 function slb_save_slb_subscriber_meta($post_id, $post, $update) {
 
+	// Is $_POST is an associative array? Where is it being accesed from??
 	// Verify nonce - if not set in this post or not correct then stops it from doing anything else
-	//$_POST is an object ?? Where does it exist or come from?
 	// wp_verify_nonce takes the field the nonce is in as a fisrt argument and an action as a second argument ?? Also don't really understand this
+	// How is slb_subscriber_nonce available here when it was declared inside the scope of a separate function??
 	if( !isset( $_POST['slb_subscriber_nonce'] ) || !wp_verify_nonce( $_POST['slb_subscriber_nonce'], basename(__FILE__) ) ) {
-		// Why return $post_id instead of just returning nothing
+		// Why return $post_id instead of just returning nothing??
 		return $post_id;
 	}
 
-	// get the post type object
+	// get the post type object using the $post argument passed to the function
 	$post_type = get_post_type_object($post->post_type);
 
 	// check if the current user has permission to edit the post, if they don't return the $post_id to prevent anything from happening
@@ -240,17 +220,24 @@ function slb_save_slb_subscriber_meta($post_id, $post, $update) {
 	global $wpdb;
 
 	// Returns all current subscriber emails
-	$check_query = $wpdb->get_col('SELECT meta_value FROM wp_postmeta WHERE meta_key = "slb_email"');
+	$email_query = $wpdb->get_col('SELECT meta_value FROM wp_postmeta WHERE meta_key = "slb_email"');
 
 	$email_found = false;
 	// Checks if current entered email is in this list
-	if (in_array($email, $check_query))
+	if (in_array($email, $email_query))
 	{
 		$email_found = true;
+		// change post published message to an error message??
+	}
+
+	// Don't add subscriber info to database if the email has been found and the subscriber isn't being updated
+	if( $email_found && ($post->post_date == $post->post_modified)) {
+		return $post_id;
 	}
 
 	// Still need a pop up message to explain the error if you added a new subscriber using an email that is already on the list
 	// Still creates an empty post if you break the rules
+	// This isn't working yet... ?? Can't find an appropriate hook yet.
 
 	function publish_success_msg() {
 		echo '<div class="notice is-dismissible notice-success">
@@ -266,7 +253,7 @@ function slb_save_slb_subscriber_meta($post_id, $post, $update) {
 
 	// Only allow updates using the subscribers update functionality or the new addition of original emails (as in a new subscriber)
 	// Had to use $post->post_date != $post->post_modified to check if an existing subscriber was being modified
-	if(($post->post_date != $post->post_modified) || !$email_found){
+	if(!$email_found || ($post->post_date != $post->post_modified)){
 
 		// update post meta
 		update_post_meta($post_id, 'slb_first_name', $first_name);
@@ -285,60 +272,29 @@ function slb_save_slb_subscriber_meta($post_id, $post, $update) {
 
 			}
 		}
-		
-		add_action( 'admin_notices', 'publish_success_msg');
 	}
 	else{
 		
-		add_action( 'admin_notices', 'publish_error_msg');
 		// change post published message to an error message??
 	}
 
 }
 
-// Still need an error message for posts that are not published
-
-add_action('save_post', 'slb_save_slb_subscriber_meta', 10, 3);
-
 function slb_edit_post_change_title(){
 
 	global $post;
 
-	if($post->post_type == 'slb_subscriber') {
+	if($post && $post->post_type == 'slb_subscriber') {
 
-		// ?? Haven't properly covered filters yet -- still getting there
+		// Edits subscriber list title
 		add_filter('the_title','slb_subscriber_title',100,2);
 
 	}
 
 }
 
-// ?? Still don't understand how you can use a file name as an action? 
-add_action('admin_head-edit.php','slb_edit_post_change_title');
-
 function slb_subscriber_title($title, $post_id) {
 	$new_title = get_post_meta($post_id,'slb_first_name',true) .' '. get_post_meta($post_id,'slb_last_name',true). ' ' . get_post_meta($post_id,'slb_email',true) ;
 
 	return $new_title;
-}
-
-
-// Just playing around with adding in a submenu for the customization menu
-// Function linked to admin_menu hook in functions.php
-function register_admin_menu() : void {
-	add_theme_page(
-		'SLB', // what the plugin settings page itself is called, esc_html__ will perform a translation if required !! Ajusted heading to match Zenhub issue
-		'SLB', // what the menu tab is called, esc_html__ will perform a translation if required
-		'manage_options', // manage_options is the user role (i.e. the admin)
-		'slb_customize', // appears in the URL - http://localhost:10013/wp-admin/admin.php?page=coil_settings
-		'render_screen', // Executed to add actual content to the page
-	);
-}
-
-add_action('admin_menu', 'register_admin_menu');
-
-function render_screen(){
-	?>
-		<h1>Heading</h1>
-	<?php
 }
